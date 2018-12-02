27a8f0efc76fa78a713c2c7d22daaa2c8d40ccff
Etga İsmail ERDEM <45507641+EtgaErdem7@users.noreply.github.com>
2018-12-02 12:43:31 +0300

    Create Truck.java

---
+++ Truck.java
@@ -0,0 +1,60 @@
+package App;
+
+public class Truck {
+
+	private int x;
+	private int y;
+	private int yFinal;
+	private int xFinal;
+	private int xStart;
+	private int yStart;
+	private int compensation;
+	
+	public int getyFinal() {
+		return yFinal;
+	}
+	public void setyFinal(int yFinal) {
+		this.yFinal = yFinal;
+	}
+	public int getxFinal() {
+		return xFinal;
+	}
+	public void setxFinal(int xFinal) {
+		this.xFinal = xFinal;
+	}
+	public int getxStart() {
+		return xStart;
+	}
+	public void setxStart(int xStart) {
+		this.xStart = xStart;
+	}
+	public int getyStart() {
+		return yStart;
+	}
+	public void setyStart(int yStart) {
+		this.yStart = yStart;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getCompensation() {
+		return compensation;
+	}
+
+	public void setCompensation(int compensation) {
+		this.compensation = compensation;
+	}
+	
+	
+	
+}

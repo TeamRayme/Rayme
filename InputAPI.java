8e1cd0ce96df84bbb699916056db8dcc50cea227
Etga İsmail ERDEM <45507641+EtgaErdem7@users.noreply.github.com>
2018-12-02 12:44:28 +0300

    Update InputAPI.java

--- InputAPI.java
+++ InputAPI.java
@@ -1,21 +1,49 @@
 package App;
-
+import java.util.*;
 public class InputAPI {
 	
-	private int trafficRateGoogle;
+	private LinkedList<Integer> roadCoords = new LinkedList<Integer>();
+	private boolean isTrafficHighGoogle;
 	private int remTimeGoogle;
-	private String trafficReasonMercedes;
-	private int trafficRateMercedes;
-	private String failedEqp;
+	private int delayTimeConnCar;
+	private int trafficConnCar;
+	private boolean failedEqp;
+	private int timeToEventZone;
+	private int[][] routes;
+	private int xS,xF,yS,yF;
 	
 	
 	
 	
-	public int getTrafficRateGoogle() {
-		return trafficRateGoogle;
+	public int getxS() {
+		return xS;
 	}
-	public void setTrafficRateGoogle(int trafficRateGoogle) {
-		this.trafficRateGoogle = trafficRateGoogle;
+	public void setxS(int xS) {
+		this.xS = xS;
+	}
+	public int getxF() {
+		return xF;
+	}
+	public void setxF(int xF) {
+		this.xF = xF;
+	}
+	public int getyS() {
+		return yS;
+	}
+	public void setyS(int yS) {
+		this.yS = yS;
+	}
+	public int getyF() {
+		return yF;
+	}
+	public void setyF(int yF) {
+		this.yF = yF;
+	}
+	public boolean getisTrafficHighGoogle() {
+		return isTrafficHighGoogle;
+	}
+	public void setTrafficHighGoogle(boolean isTrafficHighGoogle) {
+		this.isTrafficHighGoogle = isTrafficHighGoogle;
 	}
 	public int getRemTimeGoogle() {
 		return remTimeGoogle;
@@ -23,24 +51,43 @@ public class InputAPI {
 	public void setRemTimeGoogle(int remTimeGoogle) {
 		this.remTimeGoogle = remTimeGoogle;
 	}
-	public String getTrafficReasonMercedes() {
-		return trafficReasonMercedes;
-	}
-	public void setTrafficReasonMercedes(String trafficReasonMercedes) {
-		this.trafficReasonMercedes = trafficReasonMercedes;
-	}
-	public int getTrafficRateMercedes() {
-		return trafficRateMercedes;
-	}
-	public void setTrafficRateMercedes(int trafficRateMercedes) {
-		this.trafficRateMercedes = trafficRateMercedes;
-	}
-	public String getFailedEqp() {
+
+	public boolean getFailedEqp() {
 		return failedEqp;
 	}
-	public void setFailedEqp(String failedEqp) {
+	public void setFailedEqp(boolean failedEqp) {
 		this.failedEqp = failedEqp;
 	}
+	public LinkedList<Integer> getRoadCoords() {
+		return roadCoords;
+	}
+	public void setRoadCoords(LinkedList<Integer> roadCoords) {
+		this.roadCoords = roadCoords;
+	}
+
+	public int getDelayTimeConnCar() {
+		return delayTimeConnCar;
+	}
+	public void setDelayTimeConnCar(int delayTimeConnCar) {
+		this.delayTimeConnCar = delayTimeConnCar;
+	}
+	public int getTrafficConnCar() {
+		return trafficConnCar;
+	}
+	public void setTrafficConnCar(int trafficConnCar) {
+		this.trafficConnCar = trafficConnCar;
+	}
+	public int getTimeToEventZone() {
+		return timeToEventZone;
+	}
+	public void setTimeToEventZone(int timeToEventZone) {
+		this.timeToEventZone = timeToEventZone;
+	}
+	public int[][] getRoutes() {
+		int[][] routes = {{0, 1, 2, 6}, {0, 3, 4, 6}, {0, 1, 5, 6}};
+		return routes;
+	}
+
 	
 	
 	
